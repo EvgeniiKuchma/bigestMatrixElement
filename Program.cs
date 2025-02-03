5f14c8e0adc97dc91b650e0d1b79d2ab702dd4d9
Евгений Кучма <desferto87@gmail.com>
2025-02-03 16:50:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace bigestMatrixElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxElement = int.MinValue;
+            int[,] matrixA = new int[10, 10];
+
+            Random random = new Random();
+
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    matrixA[i, j] = random.Next(-100, 101);
+                    Console.Write(matrixA[i,j] + "\t");
+
+                    if(matrixA[i,j] >= maxElement)
+                    {
+                        maxElement = matrixA[i,j];                       
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine(maxElement + " - max element");
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine();
+
+            for (int i = 0;i < matrixA.GetLength(0); i++)
+            {
+                for(int j = 0;  j < matrixA.GetLength(1); j++)
+                {                   
+                    if(matrixA[i,j] == maxElement)
+                    {
+                        matrixA[i,j] = 0;
+                    }
+                    Console.Write(matrixA[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("bigestMatrixElement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bigestMatrixElement")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("44e4f1eb-a5d5-425c-9336-1fb867e8af1f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
